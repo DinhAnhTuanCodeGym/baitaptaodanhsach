4c606276ece78f29bd4ddc857d43f4eb82505ff5
DinhAnhTuanCodeGym <93692598+DinhAnhTuanCodeGym@users.noreply.github.com>
2021-11-12 08:56:32 +0700

    Add files via upload

---
+++ toantu.js
@@ -0,0 +1,12 @@
+let avg =6 ;
+let kind = "";
+
+kind = (avg > 0 && avg <= 10) ?
+    (avg >= 9) ? "Xuất Sắc" :
+        (avg >= 8) ? "Giỏi" :
+            (avg >= 7) ? "Khá" :
+                (avg >= 6) ? "Trung bình khá" :
+                    (avg >= 5) ? "Trung bình" :
+                        (avg < 5) ? "Yếu" : "Điểm không hợp lệ"
+: "Điểm không hợp lệ";
+console.log(kind);
