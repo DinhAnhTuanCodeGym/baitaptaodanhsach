3ce8501d98f8fb2b0601900f6b1ab090474ad98f
DinhAnhTuanCodeGym <93692598+DinhAnhTuanCodeGym@users.noreply.github.com>
2021-11-15 14:00:22 +0700

    Add files via upload

---
+++ loop-1.js
@@ -0,0 +1,18 @@
+// for (var i =1; i <= 10; i++ ){ 
+// console.log(i);
+// }
+// for (var i =2; i <= 10; i+=2 ){ 
+// console.log(i);
+// }
+
+// let sum = 0;
+// for (var i =1; i <= 10; i++ ){ 
+// sum += i;
+// }
+// // console.log(sum);
+
+let i = 1;
+while (i <= 100){
+    console.log(i);
+    i++
+}
